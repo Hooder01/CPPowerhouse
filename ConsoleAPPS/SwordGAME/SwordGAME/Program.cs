5e1e64800442a5a6961c8f21491ff0d84fde4a4d
Hooder <hoodernew@gmail.com>
2023-12-25 23:25:31 +1300

    BETA 0.1
    New Console App
    
    Text based game on classes of swords
    
    25/12/23

---
+++ ConsoleAPPS/SwordGAME/SwordGAME/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordGAME
+{
+    class Program
+    {
+        static void Main(string[] args) // main applaction of usage through here 
+        {
+            Console.WriteLine("Clash Of Warriors And Swords"); // start up title
+
+            Console.WriteLine("\n\n  Begin Game? (yes/no)     "); // prompts user question
+
+
+            // Creates a string variable and gets user input storing it in the variable
+            string UserInput = Console.ReadLine();
+
+            if(UserInput != "no")
+            {
+                Console.WriteLine("Exiting Program!!");
+
+
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
