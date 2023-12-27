6fbb2f5c0b112ece3a36821eb1a6682ce91a67a4
Hooder <hoodernew@gmail.com>
2023-12-27 15:46:57 +1300

    V0.3
    Added:
    
    Menu options
    
    Sword classes (WIP)

--- ConsoleAPPS/SwordGAME/SwordGAME/Program.cs
+++ ConsoleAPPS/SwordGAME/SwordGAME/Program.cs
@@ -6,6 +6,39 @@ using System.Threading.Tasks;
 
 namespace SwordGAME
 {
+    /*Alright, imagine you're building a text-based game where players wield swords in epic battles. 
+    Your task is to create a class representing a sword. 
+    This class should have properties like "Name," "Damage," and "Durability." 
+    The "Damage" property determines how much harm the sword inflicts, while "Durability" indicates how much wear and tear the sword can endure before it breaks. 
+    You might want to add methods like "Attack" to simulate the sword's usage and potentially decrease its durability with each use. 
+    Bonus points if you incorporate different types of swords with unique characteristics! */
+
+    // task taken from chatGPT (non published applaction)
+
+
+    class SwordGame // parent class
+    {
+        static void SwordClass01() // method/functions rep each diffrent sword (four can be chosen by the user)
+        {
+            string Name = "Longsword"; // name of sword/blade
+
+
+        }
+
+        static void SwordClass02()
+        {
+            string Name = "Claymore";
+        }
+
+        static void SwordClass03()
+        {
+            string Name = "Rapier";
+        }
+    }
+
+
+
+
     class Program
     {
         static void Main(string[] args) // main applaction of usage through here 
@@ -18,15 +51,18 @@ namespace SwordGAME
             // Creates a string variable and gets user input storing it in the variable
             string UserInput = Console.ReadLine();
 
-            if(UserInput != "no")
+            if (UserInput == "no") // if user types "no" into console, app will close off!
             {
                 Console.WriteLine("Exiting Program!!");
-
-
+                System.Environment.Exit(0);
             }
+            else // if user "yes" the program
+                Console.WriteLine("Please select from the selection of swords below"); // will prompt user another question
+
+                Console.WriteLine("\n 1. Longsword \n 2. Claymore \n 3. Rapier"); 
 
 
-
+            SwordGame myOBJ = new SwordGame();
 
             Console.ReadLine();
         }
