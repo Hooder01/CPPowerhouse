4e3682b0cdd2683e41c95d5da07b8462f1fb8ad7
Hooder <hoodernew@gmail.com>
2024-01-01 13:42:09 +1300

    BETA 0.1
    Program that will take an array and the console must find the needle within said array.

---
+++ ConsoleAPPS/Needle_Haystack_ArrayProgram/Needle_Haystack_ArrayProgram/Needle_Haystack_ArrayProgram.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std; // Enables access to Standard Library functions and objects //
+
+
+string findNeedle() // User defined String function for finding a needle in a Haystack // 
+{
+    string haystack[10]{ "hay" , "hay" , "junk" , "hay" , "junk" , "junk" , "hay" , "needle!" , "hay" , "hay" };
+    // an array holding ten objects 
+}
+
+
+
+int main()
+{
+    
+}
+
+
