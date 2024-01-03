2bf68ca85be73f128ed27be46aeaa99483c7f6f4
Hooder <hoodernew@gmail.com>
2024-01-03 14:12:33 +1300

    BETA 0.3
    Changes:
    
    Array being rewritten in parameters to be passed down to main function

--- ConsoleAPPS/Needle_Haystack_ArrayProgram/Needle_Haystack_ArrayProgram/Needle_Haystack_ArrayProgram.cpp
+++ ConsoleAPPS/Needle_Haystack_ArrayProgram/Needle_Haystack_ArrayProgram/Needle_Haystack_ArrayProgram.cpp
@@ -2,20 +2,16 @@
 using namespace std; // Enables access to Standard Library functions and objects //
 
 
-string findNeedle() // User defined String function for finding a needle in a Haystack // 
+void findNeedle(string haystack[] , string sizeOFhaystack) // void function holding two array parameters
 {
-    string haystack[10]{ "hay" , "hay" , "junk" , "hay" , "junk" , "junk" , "hay" , "needle!" , "hay" , "hay" };
-    // an array holding ten objects 
 
-    return 0;
 }
+
 
 
 int main()
 {
-    findNeedle(string haystack[10]);
-
-    cout << "\n" << haystack[8] -- > "found the needle at position 8";
+    
 }
 
 
