6f782535d02f4d7ec8ce436c47ac8f004a26e17a
Hooder <hoodernew@gmail.com>
2024-01-04 13:52:01 +1300

    BETA 0.3 (Unfinished)
    My ADD ass finding this hard and boring
    
    gonna look for something even simpler

---
+++ ConsoleAPPS/Needle_Haystack_ArrayProgram (Unfinished)/Needle_Haystack_ArrayProgram/Needle_Haystack_ArrayProgram.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; // Enables access to Standard Library functions and objects //
+
+
+void findNeedle(string haystack[] , string sizeOFhaystack) // void function holding two array parameters
+{
+
+}
+
+
+
+int main()
+{
+    
+}
+
+
