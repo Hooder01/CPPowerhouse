970f81a8c368119183c144061a86d567614eef49
Hooder <hoodernew@gmail.com>
2024-01-20 00:21:40 +1300

    New ConsoelApp
    to do:
    
    figure out the for loop theory

---
+++ ConsoleAPPS/FundmentalsTestScore/FundmentalsTestScore/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/* There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False! */
+
+
+namespace FundmentalsTestScore
+{
+    public class program // global open class
+    {
+        static void Main(string[] args)
+        {
+            int[] classScores = { 23, 45, 10, 16, 54 }; // class mates scores
+
+            for ()
+
+
+
+            int userScore = 48; // user (aka you) score
+
+            
+
+            Console.ReadLine(); // Debug
+        }
+        
+    }
+
+  
+}
