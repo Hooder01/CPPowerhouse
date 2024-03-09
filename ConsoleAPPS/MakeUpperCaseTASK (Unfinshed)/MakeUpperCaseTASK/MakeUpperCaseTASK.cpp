b3444e628bf649f10c9afdf7067ed59abf107334
Hooder <hoodernew@gmail.com>
2024-03-09 17:50:38 +1300

    no updates

---
+++ ConsoleAPPS/MakeUpperCaseTASK (Unfinshed)/MakeUpperCaseTASK/MakeUpperCaseTASK.cpp
@@ -0,0 +1,17 @@
+// Write a function which converts the input string to uppercase
+
+#include <iostream>
+
+
+void function() // user defined function
+{
+    std::cout << "_Automatic UpperCase Maker_"; // title
+
+    std::cout << "Please input your sentanence in lowercase"
+}
+
+int main() // main function that will run user defined function program
+{
+    function();
+}
+
