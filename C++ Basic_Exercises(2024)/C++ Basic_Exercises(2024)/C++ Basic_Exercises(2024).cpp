65c93de7a37b41af2e9eba9d34a26ec948c86fc6
Hooder <hoodernew@gmail.com>
2024-03-30 18:37:55 +1300

    Update 0.1
    Includes first completed challenge from
    w3resource.

---
+++ C++ Basic_Exercises(2024)/C++ Basic_Exercises(2024)/C++ Basic_Exercises(2024).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+
+/* 1. Write a program in C++ to print welcome text on a separate line.
+int main()
+{
+    std::cout << "\n" << "Welcome!!" << "\n";
+}*/
+
+int main() 
+{
+
+}
+
+
