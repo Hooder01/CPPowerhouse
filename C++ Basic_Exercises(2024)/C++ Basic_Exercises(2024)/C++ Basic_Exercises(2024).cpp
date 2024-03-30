3c1c2fffb76ece4bc3e84ba1fbea23ba624b4f4d
Hooder <hoodernew@gmail.com>
2024-03-30 19:10:35 +1300

    Update 0.2
    Now Includes second task (complete)

--- C++ Basic_Exercises(2024)/C++ Basic_Exercises(2024)/C++ Basic_Exercises(2024).cpp
+++ C++ Basic_Exercises(2024)/C++ Basic_Exercises(2024)/C++ Basic_Exercises(2024).cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 
 
-
 /* 1. Write a program in C++ to print welcome text on a separate line.
 int main()
 {
     std::cout << "\n" << "Welcome!!" << "\n";
 }*/
 
+// 2. Write a program in C++ to print the sum of two numbers
 int main() 
 {
+    int userinput1;
+    int userinput2;
 
+    std::cout << "Enter your first number"; std::cin >> userinput1;
+    std::cout << "Enter your second number"; std::cin >> userinput2;
+
+    std::cout << "...LOADING...";
+    std::cout << "\n" << "---------------------------------" << "\n";
+
+    std::cout << "The sum of these numbers are" << " " << userinput1 + userinput2;
+    
 }
 
 
