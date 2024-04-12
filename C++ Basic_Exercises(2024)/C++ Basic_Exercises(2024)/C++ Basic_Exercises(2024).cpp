241b879987eca8eb86e49de11c53633a1f26d73a
Hooder <hoodernew@gmail.com>
2024-04-12 14:12:29 +1200

    V5.0
    Finished task 05

--- C++ Basic_Exercises(2024)/C++ Basic_Exercises(2024)/C++ Basic_Exercises(2024).cpp
+++ C++ Basic_Exercises(2024)/C++ Basic_Exercises(2024)/C++ Basic_Exercises(2024).cpp
@@ -55,14 +55,35 @@ int main()
 }*/
 
 
-// 5. Write a in C++ program to check the upper and lower limits of integers
+/* 5. Write a in C++ program to check the upper and lower limits of integers
 
 int main() 
 {
     std::cout << "Check the upper and lower limits of integer :\n" << "---------------------------------------" << std::endl;
 
-    std::cout << "The maximum limit of (int) data type : " << INT_MAX
+    std::cout << "The maximum limit of (int) data type : " << INT_MAX << std::endl;
+    std::cout << " The minimum limit of int data type :                  " << INT_MIN << std::endl;
+    std::cout << " The maximum limit of unsigned int data type :         " << UINT_MAX << std::endl;
+    std::cout << " The maximum limit of long long data type :            " << LLONG_MAX << std::endl;
+    std::cout << " The minimum limit of long long data type :            " << LLONG_MIN << std::endl;
+    std::cout << " The maximum limit of unsigned long long data type :   " << ULLONG_MAX << std::endl;
+    std::cout << " The Bits contain in char data type :                  " << CHAR_BIT << std::endl;
+    std::cout << " The maximum limit of char data type :                 " << CHAR_MAX << std::endl;
+    std::cout << " The minimum limit of char data type :                 " << CHAR_MIN << std::endl;
+    std::cout << " The maximum limit of signed char data type :          " << SCHAR_MAX << std::endl;
+    std::cout << " The minimum limit of signed char data type :          " << SCHAR_MIN << std::endl;
+    std::cout << " The maximum limit of unsigned char data type :        " << UCHAR_MAX << std::endl;
+    std::cout << " The minimum limit of short data type :                " << SHRT_MIN << std::endl;
+    std::cout << " The maximum limit of short data type :                " << SHRT_MAX << std::endl;
+    std::cout << " The maximum limit of unsigned short data type :       " << USHRT_MAX << std::endl;
 
+}*/
+
+
+// Write a C++ program that checks whether primitive values cross the limit
+
+int main() 
+{
 
 }
 
