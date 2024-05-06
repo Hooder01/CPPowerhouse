80c4b66edd3f78e52ed3fe2fb759449be7d3bef3
*Hooder <hoodernew@gmail.com>
2024-05-06 21:01:44 +1200

    New task
    Will finish tomrrow

---
+++ ConsoleAPPS/SentanceSmashCODEWARS/SentanceSmash/SentanceSmash/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentanceSmash
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Sentance01 = { "Hello", "world", "this", "is", "great" };
+
+            string word1 = Sentance01[0];
+        }
+        
+    }
+}
