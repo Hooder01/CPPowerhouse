06ca13ef35cc814e241bbdc0a9ece458988fc9f0
*Hooder <hoodernew@gmail.com>
2024-05-07 21:42:12 +1200

    needs finishing

--- ConsoleAPPS/SentanceSmashCODEWARS/SentanceSmash/SentanceSmash/Program.cs
+++ ConsoleAPPS/SentanceSmashCODEWARS/SentanceSmash/SentanceSmash/Program.cs
@@ -14,6 +14,14 @@ namespace SentanceSmash
             string[] Sentance01 = { "Hello", "world", "this", "is", "great" };
 
             string word1 = Sentance01[0];
+            string word2 = Sentance01[1];
+            string word3 = Sentance01[2];
+            string word4 = Sentance01[3];
+            string word5 = Sentance01[4];
+
+            Console.WriteLine(word1, word2 , word3 , word4 , word5);
+
+            Console.ReadLine();
         }
         
     }
