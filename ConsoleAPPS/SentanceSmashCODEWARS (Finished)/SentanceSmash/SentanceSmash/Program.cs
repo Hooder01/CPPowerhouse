138fe46363673b7be2d0e261de9d2a07a66a2469
*Hooder <hoodernew@gmail.com>
2024-05-08 22:09:26 +1200

    Finished ver

---
+++ ConsoleAPPS/SentanceSmashCODEWARS (Finished)/SentanceSmash/SentanceSmash/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentanceSmash
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Sentance01 = { "Hello", " world", " this", " is", " great" };
+
+
+            Console.WriteLine(Sentance01[0] + Sentance01[1] + Sentance01[2] + Sentance01[3] + Sentance01[4]);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
